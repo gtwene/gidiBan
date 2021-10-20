6bbed72062d123aab0decc4bb6f139e6c473223b
FlutLab.io <>
2021-10-20 09:21:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,21 @@
+import 'package:chat/screens/welcome/welcome_screen.dart';
+import 'package:chat/theme.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Over Screens',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-//      home: MainPage(),
-
-      // Declare routes
-      routes: {
-        // Main initial route
-        '/': (context) => MainPage(),
-        // Second route
-        '/second': (context) => SecondPage(),
-      },
-      initialRoute: '/',
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Navigation over screens'),
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              // Navigate using simple push method
-              RaisedButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                ),
-                child: Text('Navigate using push method'),
-              )
-            ],
-          ),
-        ),
-      );
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second screen'),
-      ),
-      body: Container(
-        child: RaisedButton(
-          onPressed: () => Navigator.pop(context),
-          child: Text('Back'),
-        ),
-      ),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      home: WelcomeScreen(),
     );
   }
 }
